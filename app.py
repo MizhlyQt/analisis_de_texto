4ef84193da63dc98ae70af7661ef8c3a165b75ef
Angela Alvarez <angelamariaalag@gmail.com>
2025-05-01 12:16:02 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 from textblob import TextBlob
 import re
 from googletrans import Translator
+from streamlit_lottie import st_lottie
+import json
 
 # Configuración de la página
 st.set_page_config(
