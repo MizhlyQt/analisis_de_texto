4bc05b0b7838eb886cde80f05a59153c05f558d4
Angela Alvarez <angelamariaalag@gmail.com>
2025-05-01 12:28:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -33,7 +33,40 @@ modo = st.sidebar.selectbox(
 def contar_palabras(texto):
     stop_words = set([
         # Lista combinada español/inglés...
-        "a", "al", "como", "con", "el", "la", "los", "de", "y", "que", "the", "and", "is", "are", "to", "in", "it", "you", "for", "on", "with", "as", "i", "he", "she", "we", "they"
+        "a", "al", "algo", "algunas", "algunos", "ante", "antes", "como", "con", "contra",
+        "cual", "cuando", "de", "del", "desde", "donde", "durante", "e", "el", "ella",
+        "ellas", "ellos", "en", "entre", "era", "eras", "es", "esa", "esas", "ese",
+        "eso", "esos", "esta", "estas", "este", "esto", "estos", "ha", "había", "han",
+        "has", "hasta", "he", "la", "las", "le", "les", "lo", "los", "me", "mi", "mía",
+        "mías", "mío", "míos", "mis", "mucho", "muchos", "muy", "nada", "ni", "no", "nos",
+        "nosotras", "nosotros", "nuestra", "nuestras", "nuestro", "nuestros", "o", "os", 
+        "otra", "otras", "otro", "otros", "para", "pero", "poco", "por", "porque", "que", 
+        "quien", "quienes", "qué", "se", "sea", "sean", "según", "si", "sido", "sin", 
+        "sobre", "sois", "somos", "son", "soy", "su", "sus", "suya", "suyas", "suyo", 
+        "suyos", "también", "tanto", "te", "tenéis", "tenemos", "tener", "tengo", "ti", 
+        "tiene", "tienen", "todo", "todos", "tu", "tus", "tuya", "tuyas", "tuyo", "tuyos", 
+        "tú", "un", "una", "uno", "unos", "vosotras", "vosotros", "vuestra", "vuestras", 
+        "vuestro", "vuestros", "y", "ya", "yo",
+        # Inglés
+        "a", "about", "above", "after", "again", "against", "all", "am", "an", "and", 
+        "any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being", 
+        "below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't", 
+        "did", "didn't", "do", "does", "doesn't", "doing", "don't", "down", "during", 
+        "each", "few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", 
+        "haven't", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", 
+        "hers", "herself", "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", 
+        "i'm", "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its", "itself", 
+        "let's", "me", "more", "most", "mustn't", "my", "myself", "no", "nor", "not", 
+        "of", "off", "on", "once", "only", "or", "other", "ought", "our", "ours", 
+        "ourselves", "out", "over", "own", "same", "shan't", "she", "she'd", "she'll", 
+        "she's", "should", "shouldn't", "so", "some", "such", "than", "that", "that's", 
+        "the", "their", "theirs", "them", "themselves", "then", "there", "there's", 
+        "these", "they", "they'd", "they'll", "they're", "they've", "this", "those", 
+        "through", "to", "too", "under", "until", "up", "very", "was", "wasn't", "we", 
+        "we'd", "we'll", "we're", "we've", "were",         "weren't", "what", "what's", "when", 
+        "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", 
+        "why's", "with", "would", "wouldn't", "you", "you'd", "you'll", "you're", "you've",
+        "your", "yours", "yourself", "yourselves"
     ])
     palabras = re.findall(r'\b\w+\b', texto.lower())
     palabras_filtradas = [p for p in palabras if p not in stop_words and len(p) > 2]
